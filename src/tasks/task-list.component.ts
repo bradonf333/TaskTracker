494e0d4e79d61204d1617197f092d6a665cff31d
bradonf333 <bradonf123@gmail.com>
2017-08-06 21:04:26 -0600

    Commented out the date property for now. Added hard coded tasks also.

---
+++ src/tasks/task-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ITask } from './task';
+
+@Component({
+    moduleId: module.id,
+    selector: 'selector',
+    // templateUrl: './name.component.html',
+    // styleUrls: ['./name.component.css']
+})
+export class TaskListComponent implements OnInit {
+
+    tasks: ITask[] = [
+        {
+            'taskId': 1,
+            'language': 'Angular 2',
+            'hours': 2,
+            'taskDescription': 'Started creating this task scheduler',
+            // 'date': "August 6, 2017 8:50:00"
+        },
+        {
+            'taskId': 2,
+            'language': 'Angular 2',
+            'hours': 2,
+            'taskDescription': 'Created another task.',
+            // 'date': "August 6, 2017 8:50:00"
+        }
+    ];
+    ngOnInit(): void {
+
+    }
+}
