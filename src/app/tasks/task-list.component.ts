e0aa34d1bb275567744424644301a83c87971778
bradonf333 <bradonf123@gmail.com>
2017-08-07 21:09:21 -0600

    Added some images for languages

--- src/app/tasks/task-list.component.ts
+++ src/app/tasks/task-list.component.ts
@@ -10,21 +10,28 @@ import { ITask } from './task';
 })
 export class TaskListComponent implements OnInit {
 
-    pageTitle: string = 'Task List';
+    pageTitle: string = 'Programming Task List';
     tasks: ITask[] = [
         {
             'taskId': 1,
             'language': 'Angular 2',
             'hours': 2,
             'taskDescription': 'Started creating this task scheduler',
-            'date': 'August 6, 2017 8:50:00'
+            'date': 'August 6, 2017 8:50 PM'
         },
         {
             'taskId': 2,
             'language': 'Angular 2',
             'hours': 2,
             'taskDescription': 'Created another task.',
-            'date': 'August 6, 2017 8:50:00'
+            'date': 'August 6, 2017 8:50 PM'
+        },
+        {
+            'taskId': 3,
+            'language': 'Angular 2',
+            'hours': 1.5,
+            'taskDescription': 'Got the main stuff working for the tasks',
+            'date': 'August 7, 2017 8:41 PM'
         }
     ];
 
