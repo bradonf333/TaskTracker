9151a4641d459686b9136a55e384d729ec8efedd
bradonf333 <bradonf123@gmail.com>
2017-08-09 18:50:03 -0600

    Added TaskFilterPipe to declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './tasks/task-list.component';
 import { TaskService } from './tasks/task.service';
 import { HttpModule } from '@angular/http';
+import { TaskFilterPipe } from './tasks/task-filter.pipe';
 
 @NgModule({
   imports: [BrowserModule, HttpModule ]
-  , declarations: [AppComponent, TaskListComponent]
+  , declarations: [AppComponent, TaskListComponent, TaskFilterPipe]
   , providers: [TaskService]
   , bootstrap: [AppComponent]
 })
