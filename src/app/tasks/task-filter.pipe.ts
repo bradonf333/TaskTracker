7ecb2e2073641c38923c94d5fcf2e0a898c931cc
bradonf333 <bradonf123@gmail.com>
2017-08-09 18:49:09 -0600

    Added a filter by pipe to filter tasks by language

---
+++ src/app/tasks/task-filter.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { ITask } from './task';
+
+/**
+ * Custom pipe used to filter an array of Tasks by the Task Name
+ */
+@Pipe({name: 'taskFilter'})
+export class TaskFilterPipe implements PipeTransform {
+
+    transform(value: ITask[], filterBy: string): ITask[] {
+
+        // Checks to see if the filterBy param is defined. If not then set to null
+        filterBy = filterBy ? filterBy.toLocaleLowerCase() : null;
+
+        // Filters the tasks by the filterBy parameter string passed in
+        return filterBy ? value.filter((task: ITask) =>
+            task.language.toLocaleLowerCase().indexOf(filterBy) !== -1) : value;
+
+    }
+}
