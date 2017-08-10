54c0ea19b0c46a2ee8daf0b78cba35dae872c865
bradonf333 <bradonf123@gmail.com>
2017-08-09 21:21:11 -0600

    Set up templateUrl for app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `
-    <div>
-        <nav class='navbar navbar-default'>
-            <div class='container-fluid'>
-                <a class='navbar-brand'>{{name}}</a>
-                <ul class='nav navbar-nav'>
-                    <!--<li><a [routerLink]="['/welcome']">Home</a></li>-->
-                    <li><a [routerLink]="['/tasks']">Task List</a></li>
-                </ul>
-            </div>
-        </nav>
-        <div class ='container'>
-            <router-outlet></router-outlet>
-        </div>
-    </div>
-  ` ,
+    moduleId: module.id,
+    selector: 'my-app',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
 export class AppComponent { name = 'Task Manager'; }
