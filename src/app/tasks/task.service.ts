4c77212216006f5e2c08ddea32ca4a707321fa48
bradonf333 <bradonf123@gmail.com>
2017-08-10 20:44:07 -0600

    added method to get languages from json

--- src/app/tasks/task.service.ts
+++ src/app/tasks/task.service.ts
@@ -12,6 +12,9 @@ export class TaskService {
     // Represents a web api url that would have access to the list of tasks
     private _taskUrl = './api/tasks/tasks.json';
 
+    // Represents a web api url that would have access to the list of languages
+    private _languageUrl = './api/tasks/task-languages.json';
+
     /**
      * Constructor to inject the Http Service from Angular
      */
@@ -25,6 +28,17 @@ export class TaskService {
     getTasks(): Observable<ITask[]> {
         return this._http.get(this._taskUrl)
             .map((response: Response) => <ITask[]>response.json())
+            .do(data => console.log('All: ' + JSON.stringify(data)))
+            .catch(this.handleError);
+    }
+
+    /** 
+     * Returns a list of languages by sending an http request to the 
+     * given url and casting the json response into a string
+     */
+    getLanguages(): Observable<string[]> {
+        return this._http.get(this._languageUrl)
+            .map((response: Response) => <string[]>response.json())
             .do(data => console.log('All: ' + JSON.stringify(data)))
             .catch(this.handleError);
     }
