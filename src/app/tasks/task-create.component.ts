7ef505460eae9674cc0ab83823763b723b4000ea
bradonf333 <bradonf123@gmail.com>
2017-08-10 21:43:25 -0600

    added service to get languages and display in the form

--- src/app/tasks/task-create.component.ts
+++ src/app/tasks/task-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TaskService } from './task.service';
 
 @Component({
     moduleId: module.id,
@@ -9,12 +10,26 @@ import { Router } from '@angular/router';
 export class TaskCreateComponent implements OnInit {
 
     languages: string[];
+    errorMessage: string;
 
     pageTitle: string = 'Create a new Task';
 
-    constructor(private _router: Router) { }
+    constructor(
+        private _router: Router,
+        private _taskService: TaskService
+    ) { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        /** Calls the service to return a list of products */
+        this._taskService.getLanguages()
+            .subscribe(languages => {
+                this.languages = languages;
+                // this.tasks.forEach(element => {
+                //     this.setImageUrl(element);
+                // });
+            },
+            error => this.errorMessage = <any>error);
+    }
 
     onBack(): void {
         this._router.navigate(['/tasks']);
